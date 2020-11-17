4dfae11744993922cbc2b9f6e2661d353c2b6e39
Mikhail Kurenkov <kurenkov1995@mail.ru>
2020-11-17 17:07:44 +0300

    #11: new activation function

--- mvae/models/utils.py
+++ mvae/models/utils.py
@@ -1,6 +1,12 @@
 import torch.nn as nn
 
 
+class Swish(nn.Module):
+    """https://arxiv.org/abs/1710.05941"""
+    def forward(self, x):
+        return x * nn.functional.sigmoid(x)
+
+
 def activation_function(activation_type):
     if activation_type == "relu":
         return nn.ReLU()
@@ -8,5 +14,7 @@ def activation_function(activation_type):
         return nn.Tanh()
     elif activation_type == "leaky_relu":
         return nn.LeakyReLU()
+    elif activation_type == "swish":
+        return Swish()
     else:
         raise ValueError(f"Unknown activation type {activation_type}")
